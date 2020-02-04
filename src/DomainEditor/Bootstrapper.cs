f6f0252ffc5f868b71af5246dc9542654c5d2abd
Cagri Özer <cagri.ozer@coolblue.nl>
2020-02-04 23:52:20 +0100

    Add UseCase Bootstrapper

---
+++ src/DomainEditor/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using DomainEditor.UseCases;
+using DomainEditor.UseCases.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DomainEditor
+{
+    public static class Bootstrapper
+    {
+        public static void AddUseCases(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<ICreateDomainUseCase, CreateDomainUseCase>();
+            serviceCollection.AddTransient<IRetrieveDomainsUseCase, RetrieveDomainsUseCase>();
+        }
+    }
+}
